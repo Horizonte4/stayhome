f49b4d4334ec3b43bb03a081a0dc8b6c81b78b53
Laura <lmarinvelez0@gmail.com>
2026-03-10 18:57:16 -0500

    fix(auth):register view has been change to a function

--- users/urls.py
+++ users/urls.py
@@ -2,25 +2,21 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (
     home,
-    RegisterView,
+    register_view,
     login_view,
     board,
     edit_profile,
     logout_view
 )
 
+#LAURA:
+# Aquí definimos las URLs para las vistas de usuarios.
+
 urlpatterns = [
     path('', home, name='home'),
-    path('registration/', RegisterView.as_view(), name='registration'),
+    path('registration/', register_view, name='registration'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('board/', board, name='board'),
     path('edit_profile/', edit_profile, name='edit_profile'),
-
-
-    #path('dashboard/client/', dashboard_client, name='dashboard_client'),
-    #path('dashboard/owner/', dashboard_owner, name='dashboard_owner'),
-    
-
-    #path('profile/', ProfileView.as_view(), name='profile'),
 ]
