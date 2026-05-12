d831a2b339bdcccd9c27e847719f0e305b25f77e
Laura <lmarinvelez0@gmail.com>
2026-05-12 10:24:55 -0500

    style: ruff format

--- transactions/selectors.py
+++ transactions/selectors.py
@@ -1,8 +1,5 @@
 from .models import Booking, Purchase
 
-from django.conf import settings
-from datetime import timedelta
-from django.utils import timezone
 
 
 def get_client_bookings_context(user):
