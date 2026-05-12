be273002c8086a721ff8c28bcf8381dbc775109a
Laura <lmarinvelez0@gmail.com>
2026-05-12 10:21:23 -0500

    refactor: muevo can cancel al modelo

--- transactions/selectors.py
+++ transactions/selectors.py
@@ -1,6 +1,7 @@
 from .models import Booking, Purchase
 
 from django.conf import settings
+from datetime import timedelta
 from django.utils import timezone
 
 
@@ -8,10 +9,3 @@ def get_client_bookings_context(user):
     context = Booking.objects.client_context(user)
     context["purchased_properties"] = Purchase.objects.for_buyer(user)
     return context
-
-
-@property
-def can_cancel(self):
-    today = timezone.localdate()
-    cancel_days_limit = getattr(settings, "BOOKING_CANCEL_DAYS_LIMIT", 5)
-    return (self.check_in - today).days > cancel_days_limit
