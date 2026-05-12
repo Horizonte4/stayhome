c7c26f1fab3be646af8e25a3693e458d19c5ab2e
Laura <lmarinvelez0@gmail.com>
2026-05-12 10:29:21 -0500

    style: ruff check

--- transactions/selectors.py
+++ transactions/selectors.py
@@ -1,7 +1,6 @@
 from .models import Booking, Purchase
 
 
-
 def get_client_bookings_context(user):
     context = Booking.objects.client_context(user)
     context["purchased_properties"] = Purchase.objects.for_buyer(user)
